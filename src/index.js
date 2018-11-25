35800828baa4e108c9a0ac387018c73cc0ee1708
Luc Patiny <luc@patiny.com>
2018-11-25 07:41:59 +0100

    refactor and tests

--- src/index.js
+++ src/index.js
@@ -1,69 +1,71 @@
 'use strict';
 
 const analyseMF = require('chemcalc').analyseMF;
-const formatChemcalcResult = require('./formatChemcalcResult');
+const processMF = require('./processMF');
 const gnuplot = require('gnuplot');
 const fs = require('fs');
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_TOKEN;
 
-let bot = new TelegramBot(token, {polling: true});
+let bot = new TelegramBot(token, { polling: true });
 
 // A very polite bot
-bot.getMe().then((me) => {
+bot.getMe().then(me => {
   console.log('Hi my name is %s!', me.username);
 });
 
 // Inline rendering
 // @chemcalc_bot C10H12O3
-bot.on('inline_query', (msg) => {
+bot.on('inline_query', msg => {
   let ans, res_text;
   try {
-    ans = analyseMF(msg.query, {isotopomers: 'arrayXYXY'});
+    ans = analyseMF(msg.query, { isotopomers: 'arrayXYXY' });
     res_text = formatChemcalcResult(ans, true, false);
   } catch (error) {
     if (error.b === 'Isotopic distribution: empty table') {
       ans = analyseMF(msg.query);
       res_text = formatChemcalcResult(ans, true, true);
     } else {
-      ans = {mv: 'not found'};
+      ans = { mv: 'not found' };
       res_text = error.b;
     }
   }
 
-  bot.answerInlineQuery(msg.id, [{
-    type: 'article',
-    id: '1',
-    title: msg.query + ' - mw: ' + ans.mw,
-    input_message_content: {
-      message_text: msg.query ? res_text : 'Molecular formula not given',
-      parse_mode: 'Markdown'
+  bot.answerInlineQuery(msg.id, [
+    {
+      type: 'article',
+      id: '1',
+      title: msg.query + ' - mw: ' + ans.mw,
+      input_message_content: {
+        message_text: msg.query ? res_text : 'Molecular formula not given',
+        parse_mode: 'Markdown'
+      }
     }
-  }]);
+  ]);
 });
 
 // Not inline rendering
 // C10H12O3
 bot.onText(/(^[^\/@]+)/, (msg, match) => {
-
   // formula calculation
   let fromId = msg.from.id;
   let result;
   try {
-    result = chem_calc(match[1]);
-    bot.sendMessage(fromId, result.data, {parse_mode: 'Markdown'});
+    result = processMF(match[1]);
+    bot.sendMessage(fromId, result.data, { parse_mode: 'Markdown' });
 
     // image rendering
-    img_generation(result, fromId, (err) => {
+    img_generation(result, fromId, err => {
       if (err) console.error(err);
       bot.sendPhoto(fromId, fromId + '.png');
     });
   } catch (error) {
     if (error.b === 'Isotopic distribution: empty table') {
-      bot.sendMessage(fromId,
-          formatChemcalcResult(analyseMF(match[1]), false, true),
-          {parse_mode: 'Markdown'}
+      bot.sendMessage(
+        fromId,
+        formatChemcalcResult(analyseMF(match[1]), false, true),
+        { parse_mode: 'Markdown' }
       );
     } else {
       bot.sendMessage(fromId, error.b);
@@ -72,31 +74,12 @@ bot.onText(/(^[^\/@]+)/, (msg, match) => {
 });
 
 /**
- * Make the MF analysis
- * @param  {string} formula chemical formula
- * @return {object} formated result, x values and y values
- */
-function chem_calc (formula) {
-  let result = analyseMF(formula, {isotopomers: 'arrayXYXY'});
-  let xy = result.arrayXYXY;
-  let chartXFrom = Math.floor(xy[0][0]) - 2;
-  let chartXTo = Math.ceil(xy[xy.length - 1][0]) + 2;
-
-  return {
-    data: formatChemcalcResult(result, false, false),
-    xy: xy,
-    chartXFrom: chartXFrom,
-    chartXTo: chartXTo
-  };
-}
-
-/**
  * Generates an Isotopic distribution image
  * @param {object} result - resulting object of the che_calc function
  * @param {number} fromId - id of the user that requested the formula
  * @param {function} callback what to do when the img is generated
  */
-function img_generation (result, fromId, callback) {
+function img_generation(result, fromId, callback) {
   let out = fs.createWriteStream(fromId + '.png');
   let plot = gnuplot()
     .set('terminal png medium size 400,300')
@@ -112,10 +95,18 @@ function img_generation (result, fromId, callback) {
     .set('xrange [' + result.chartXFrom + ':' + result.chartXTo + ']')
     .set('yrange [0:120]');
 
-  result.xy.forEach( (point) => {
-    plot.set('arrow from ' + point[0] + ',0 to ' + point[0] + ',' + point[1] * 100 + ' nohead lw 2 lc rgb "blue"');
+  result.xy.forEach(point => {
+    plot.set(
+      'arrow from ' +
+        point[0] +
+        ',0 to ' +
+        point[0] +
+        ',' +
+        point[1] * 100 +
+        ' nohead lw 2 lc rgb "blue"'
+    );
   });
-  plot.plot('sin(x) lc rgb "white"', {end: true}).pipe(out);
+  plot.plot('sin(x) lc rgb "white"', { end: true }).pipe(out);
 
   out.on('finish', callback);
 }
---
+++ src/processMF.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const analyseMF = require('chemcalc').analyseMF;
+
+const url = 'http://www.chemcalc.org/analyse_content?mf=';
+
+/**
+ * Make the MF analysis
+ * @param  {string} formula chemical formula
+ * @return {object} formated result, x values and y values
+ */
+function processMF(formula) {
+  let result = analyseMF(formula, {isotopomers: 'arrayXYXY'});
+  let xy = result.arrayXYXY;
+  let chartXFrom = Math.floor(xy[0][0]) - 2;
+  let chartXTo = Math.ceil(xy[xy.length - 1][0]) + 2;
+
+  return {
+    data: formatResult(result, false, false),
+    xy: xy,
+    chartXFrom: chartXFrom,
+    chartXTo: chartXTo
+  };
+}
+
+function formatResult(chemcalcResult, link, emptyTable) {
+  let result = [];
+  result.push('*MF:* ' + chemcalcResult.mf);
+  result.push('*MW:* ' + chemcalcResult.mw);
+  result.push('*EM:* ' + chemcalcResult.em.toFixed(5));
+
+  if (chemcalcResult.parts && chemcalcResult.parts.length == 1) {
+    if (chemcalcResult.parts[0].msem &&
+        chemcalcResult.parts[0].msem != chemcalcResult.em) {
+      result.push('*Exp.&nbsp;EM:* ' + chemcalcResult.parts[0].msem.toFixed(5));
+    }
+    if (chemcalcResult.parts[0].unsaturation) {
+      result.push('*Unsat.:* ' + chemcalcResult.parts[0].unsaturation);
+    }
+  }
+
+  if (chemcalcResult.parts.length == 1) {
+    // Elemental analysis
+    result.push(' ');
+    result.push('*Element* \t *Count* \t *Percent*');
+    for (let i = 0; i < chemcalcResult.parts[0].ea.length; ++i) {
+      result.push(
+          chemcalcResult.parts[0].ea[i].element + ' \r\t ' +
+          chemcalcResult.parts[0].ea[i].number + ' \r\t ' +
+          chemcalcResult.parts[0].ea[i].percentage.toFixed(3) + '%');
+    }
+  }
+
+  if (link) {
+    result.push(' ');
+    result.push('For more info click [here](' + url + chemcalcResult.mf + ')');
+  }
+
+  if (emptyTable) {
+    result.push(' ');
+    result.push('_Isotopic distribution not found_');
+  }
+
+  return result.join('\r\n');
+};
+
+module.exports = processMF;
