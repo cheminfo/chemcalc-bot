3589532950a509c6f159808698130ecf58b78913
Luc Patiny <luc@patiny.com>
2023-02-06 16:46:00 +0100

    chore: wip migrating image generation

--- docker/src/utils/generateIsotopicDistributionImage.js
+++ docker/src/utils/generateIsotopicDistributionImage.js
@@ -1,6 +1,8 @@
-import { createWriteStream } from 'fs';
+import { ChartJSNodeCanvas } from 'chartjs-node-canvas';
 
-import gnuplot from 'gnuplot';
+const width = 400; //px
+const height = 250; //px
+const backgroundColour = 'white'; // Uses https://www.w3schools.com/tags/canvas_fillstyle.asp
 
 /**
  * Generates an Isotopic distribution image
@@ -8,33 +10,87 @@ import gnuplot from 'gnuplot';
  * @param {number} fromId - id of the user that requested the formula
  * @param {function} callback what to do when the img is generated
  */
-export function generateIsotopicDistributionImage(result, fromId, callback) {
+export function generateIsotopicDistributionImage(result) {
   const peaks = result.isotopicDistribution;
   const xs = peaks.map((point) => point.x);
-  const xMin = Math.min(...xs) - 1;
-  const xMax = Math.max(...xs) + 1;
+  const xMin = Math.floor(Math.min(...xs) - 1);
+  const xMax = Math.ceil(Math.max(...xs) + 1);
 
-  let out = createWriteStream(`${fromId}.png`);
-  let plot = gnuplot()
-    .set('terminal png medium size 400,300')
-    .set('title "Isotopic distribution"')
-    .unset('key') // no legend
-    .set('border 3')
-    .set('xlabel "m/z (Da)"')
-    .set('ylabel "Intensity (%)"')
-    .set('ytics nomirror') // hide ticks left
-    .set('xtics nomirror') // hide ticks top
-    .set('grid xtics lw 1 lc rgb "#dddddd"')
-    .set('grid ytics lw 1 lc rgb "#dddddd"')
-    .set(`xrange [${xMin}:${xMax}]`)
-    .set('yrange [0:120]');
-
-  peaks.forEach((point) => {
-    plot.set(
-      `arrow from ${point.x},0 to ${point.x},${point.y} nohead lw 2 lc rgb "blue"`,
-    );
+  const chartJSNodeCanvas = new ChartJSNodeCanvas({
+    width,
+    height,
+    backgroundColour,
   });
-  plot.plot('sin(x) lc rgb "white"', { end: true }).pipe(out);
 
-  out.on('finish', callback);
+  // See https://www.chartjs.org/docs/latest/configuration
+  const configuration = {
+    type: 'scatter',
+    data: {
+      datasets: [
+        {
+          data: peaks,
+          pointRadius: 0,
+          borderWidth: 0,
+          borderColor: 'rgba(255, 0, 0, 1)',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          type: 'scatter',
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          suggestedMax: 100,
+          title: {
+            text: 'Relative intensity',
+            display: true,
+          },
+        },
+        x: {
+          min: xMin,
+          max: xMax,
+          title: {
+            text: 'm/z',
+            display: true,
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+          labels: {
+            color: 'rgb(255, 99, 132)',
+          },
+        },
+        title: {
+          display: true,
+          text: 'Isotopic distribution',
+        },
+      },
+    },
+    plugins: [VerticalLinePlugin],
+  };
+  return chartJSNodeCanvas.renderToBuffer(configuration);
+
+  // return chartJSNodeCanvas.renderToStream(configuration);
 }
+
+const VerticalLinePlugin = {
+  afterDatasetDraw: (chart, params) => {
+    // Only draw after animation has finished and is the desired dataset
+    if (params.meta.type == 'scatter') {
+      const ctx = chart.ctx;
+      ctx.save();
+      params.meta.data.forEach((point) => {
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(point.x, point.y);
+        ctx.lineTo(point.x, chart.chartArea.bottom);
+        ctx.stroke();
+      });
+      ctx.restore();
+    }
+  },
+};
